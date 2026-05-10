f5d802eceeb155a71cc033e9df391aafcb689f25
Kumar Abhinav <Kumar.Abhinav2@ibm.com>
2026-05-10 18:49:57 +0530

    conflicts fixed

---
+++ pipeline.groovy
@@ -0,0 +1,39 @@
+pipeline {
+	
+	agent any
+	parameters {
+		string(name: "fileToRun", defaultValue: "/src/test/java/Jenkins/PracticeJenkins", description: "Provide the package plus file name to run, example: /Jenkins/PracticeJenkins")
+		string(name: "mavenCommand", defaultValue: "clean test", description: "Provide the command for Maven")
+	}
+	stages {
+		stage("Configuring") {
+			steps {
+				echo "$fileToRun"
+				echo "$mavenCommand"
+				def runnerFile = sh "$fileToRun"
+				def runnerCommand = sh "$mavenCommand"
+			}
+		}
+		stage("Test") {
+			steps {
+				script {
+					sh "mvn"
+					sh "$runnerCommand"
+				}
+			}
+		}
+	}
+	
+	post {
+		success {
+			echo "Test is successful."
+		}
+		failure {
+			echo "Test failed."
+		}
+		always {
+			echo "Pipeline execution complete."
+		}
+	}
+	
+}
