3c31892f47f8c21936d57c68804a064e845707fd
Kumar Abhinav <Kumar.Abhinav2@ibm.com>
2026-05-11 11:27:50 +0530

    Adding test files

---
+++ src/test/java/Jenkins/PracticeJenkins/Runner.java
@@ -0,0 +1,12 @@
+package Jenkins.PracticeJenkins;
+
+import org.testng.annotations.Test;
+
+public class Runner {
+	
+	@Test
+	public void runner() {
+		System.out.println("Ran from Test.");
+	}
+
+}
