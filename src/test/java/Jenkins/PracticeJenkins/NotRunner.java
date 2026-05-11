5d4d14e01f15f2f8a31dd7dcb2010a4fc5687e46
Kumar Abhinav <59536541+kumarabhinv@users.noreply.github.com>
2026-05-11 11:50:22 +0530

    Adding test files (#4)
    * Adding test files
    
    * Added test files and configuration
    
    ---------
    
    Co-authored-by: Kumar Abhinav <Kumar.Abhinav2@ibm.com>

---
+++ src/test/java/Jenkins/PracticeJenkins/NotRunner.java
@@ -0,0 +1,12 @@
+package Jenkins.PracticeJenkins;
+
+import org.testng.annotations.Test;
+
+public class NotRunner {
+	
+	@Test
+	public void runner() {
+		System.out.println("Ran from Not Runner.");
+	}
+
+}
